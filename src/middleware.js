6c1a2613f3b187d58e8f73440c415060ee797a62
ktsonev <ktsonev@extensadev.com>
2023-11-07 13:33:45 +0200

    create user pages and setup middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+    if (!request.nextUrl.pathname.startsWith('/user/login')) {
+        return NextResponse.redirect(new URL('/user/login', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico|).*)',
+        '/((?!user/login))',
+        '/((?!user/logout))',
+        '/((?!user/register))'
+    ],
+}
