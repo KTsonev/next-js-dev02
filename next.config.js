a2e2cb8521e64b22ed0e2f4f4c0c85b0fbf9260a
KTsonev <Ktsonev@extensadev.com>
2023-11-09 23:53:56 +0200

    api methods

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_URL: 'https://books-library-dev.herokuapp.com/'
+    }
+}
 
 module.exports = nextConfig
