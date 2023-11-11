c036e9afbedd1a5d8b0211270c15af3ae2ddeaf3
KTsonev <Ktsonev@extensadev.com>
2023-11-11 23:02:45 +0200

    cleaning and change js to tsx

---
+++ src/app/api/book/list/route.tsx
@@ -0,0 +1,27 @@
+import {NextResponse} from 'next/server';
+import {cookies} from 'next/headers';
+
+export async function GET() {
+    const apiToken = cookies().get('api-token')?.value || '';
+
+    if (!apiToken) {
+        return NextResponse.json({
+                ok: false,
+                error: 'param missing'
+            },
+            {status: 400}
+        );
+    }
+
+    const fetchResponse = await fetch(`${process.env.API_URL}api/book`,
+        {headers: {Authorization: `Bearer ${apiToken}`}}
+    );
+
+    if (fetchResponse.status === 200) {
+        const data = await fetchResponse.json();
+        return NextResponse.json({ok: true, books: data});
+    } else {
+        const data = await fetchResponse.json();
+        return NextResponse.json({ok: false, books: []});
+    }
+}
--- src/app/user/logout/page.js
+++
@@ -1,5 +0,0 @@
-export default function LogoutPage() {
-    return (
-        <p>LogoutPage</p>
-    );
-}
